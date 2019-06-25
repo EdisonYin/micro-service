926c6bd0ca9c03632ccea39270facb1d42b5bd6f
Edison Yin <edisonyin@augmentum.com.cn>
2019-06-25 19:07:20 +0800

    Add File CSV convert Html.

---
+++ src/main/java/com/light/springboot/service/FileConvertService.java
@@ -0,0 +1,10 @@
+package com.light.springboot.service;
+
+import java.io.File;
+
+/**
+ * Created by Edison.Yin on 2019/6/25.
+ */
+public interface FileConvertService {
+    void convertCsvToHtml(File file);
+}
--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -1,16 +1,36 @@
 package com.example.demo;
 
+import com.light.springboot.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {Application.class})
 public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		class Test{
+			public String name;
+		}
+		ArrayList<String> users = new ArrayList<>(2);
+		users.add("s");
+		System.out.println("test before null");
+		String nullStr = null;
+		System.out.println("Set nullStr");
+		System.out.println(nullStr);
+		System.out.println("after nulll");
+		Test test = null;
+		System.out.println(test);
+		System.out.println(test+"after nulll");
+		System.out.println(test.name);
+		System.out.println(test+"after nulll");
 	}
+
+
 
 }
